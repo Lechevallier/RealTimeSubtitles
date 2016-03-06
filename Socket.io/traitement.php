eecd77e2c7b7346de6e8a24145a34e7cad39b296
SaraOunissi <sara.ounissi@gmail.com>
2016-03-06 17:04:36 +0100

    Connexion et session

---
+++ Socket.io/traitement.php
@@ -0,0 +1,9 @@
+<?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+
+// On s'amuse à créer quelques variables de session dans $_SESSION
+$_SESSION['prenom'] = htmlspecialchars($_POST['prenom']);
+$_SESSION['nom'] = htmlspecialchars($_POST['nom']);
+
+?>
