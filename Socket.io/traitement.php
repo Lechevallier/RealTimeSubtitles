ba1662824ce76734051a5966d809b8eff2311645
SaraOunissi <sara.ounissi@gmail.com>
2016-03-06 17:22:41 +0100

    Correction connexion

--- Socket.io/traitement.php
+++ Socket.io/traitement.php
@@ -7,3 +7,14 @@ $_SESSION['prenom'] = htmlspecialchars($_POST['prenom']);
 $_SESSION['nom'] = htmlspecialchars($_POST['nom']);
 
 ?>
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+	<title>Traitement</title>
+</head>
+<body>
+
+</body>
+</html>
