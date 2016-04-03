2dfc1ae95616e66e156d72df2f43ebf327c440db
Lechevallier <maxime.lechevallier@laposte.net>
2016-04-03 16:47:10 +0200

    ...

--- meteor/RealTimeSubtitles/RealTimeSubtitles.js
+++ meteor/RealTimeSubtitles/RealTimeSubtitles.js
@@ -28,8 +28,10 @@ if (Meteor.isClient) {
           return moment(date).fromNow();
     },
     DisplayAuthor: function(id) {
-        if(Meteor.users.find(id).fetch().toString() != []){
-          return Meteor.users.find(id).fetch()[0].emails[0].address;
+        if((Meteor.users.findOne(id) != undefined) && (Meteor.users.findOne(id).emails != undefined)){
+          return Meteor.users.findOne(id).emails[0].address;
+        }else{
+          return id;
         }
     }
   });
@@ -79,25 +81,39 @@ if (Meteor.isClient) {
     thereIsSlide: function(slideTab){
       return SlidesCollection.find({course: Session.get('joinedCourse')}).fetch().toString() != [];
     },
-    thereIsWord: function(slide){
-      return slide[0].notes.toString() != [];
+    thereIsWord: function(notes){
+      return notes.toString() != [];
     }
   });
 
 
   Template.displaySlide.events({
-    'mouseover #displaySlide': function(){
+    'mouseover .displaySlide': function(){
       Session.set('mouseOverSlide', this._id);
     },
-    'click #displaySlide': function(){
+    'click .displaySlide': function(){
       Session.set('selectedSlide', this._id);
-    }
+    },
   });
 
 
-  Template.displayWord.onRendered(function() {
+  Template.displayWord.onRendered(function(){
       this.$('.selectpicker').selectpicker();
+
   });
+
+  var renderTimeout = false;
+  Template.displayOption.onRendered(function(){
+    if (renderTimeout !== false) {
+      Meteor.clearTimeout(renderTimeout);
+    }
+    renderTimeout = Meteor.setTimeout(function() {
+      console.log('yo')
+      $('#my-select').selectpicker("refresh");
+      renderTimeout = false;
+    }, 100);
+  });
+
 
   Template.displayWord.helpers({
     displayWordCss: function(){
@@ -111,90 +127,116 @@ if (Meteor.isClient) {
     displayWord: function(idWord){
       return WordsCollection.findOne(idWord.toString()).text;
     },
-    displayScore: function(idWord){
-      return WordsCollection.findOne(idWord.toString()).score;
-    },
     selectOrSpan: function(wordOptArray){
       return (wordOptArray.length>1);
     },
-    addOpt: function(idWord){
-      return ((idWord == Session.get('wordToAddOpt')) && (idWord == Session.get('mouseOverWord')) && (idWord != Session.get('addWordForm')));
+    addWordForm: function(idWord){
+      return ((idWord == Session.get('selectedWord')) && (idWord == Session.get('mouseOverWord')) && (idWord != Session.get('insertWordForm')));
     },
     reinitialiseAddWordOpt: function(){
-      Session.set('wordToAddOpt', null);
-    }
+      Session.set('addWordForm', null);
+    },
+    reinitialiseAddWord: function(){
+      Session.set('addWordForm', null);
+    },
+    displayWord: function(idWord){
+      return WordsCollection.findOne(idWord.toString()).text;
+    },
+    displayScore: function(idWord){
+      return WordsCollection.findOne(idWord.toString()).score;
+    },
+    id: function(idWord){
+      return WordsCollection.findOne(idWord.toString())._id;
+    },
   })
+
+
+  Template.displayOption.helpers({
+
+  })
+
 
   Template.displayWord.events({
     'mouseover': function(){
       Session.set('mouseOverWord', this.var[0]);
     },
     'click': function(){
+      console.log(this)
       Session.set('selectedWord', this.var[0]);
-      Session.set('wordToAddOpt', this.var[0]);
     },
-    'dblclick': function(){
-      Session.set('dblclickWord', this.var[0]);
+    'submit form': function(event){
+      event.preventDefault();
+      if(event.target.addOptSelect != undefined){
+        console.log(event.target.addOptSelect.value);
+        Meteor.call('addOptWordData', Session.get('selectedSlide'), event.target.addOptSelect.value, Session.get('selectedWord'), function(result, error){$('.selectpicker').selectpicker('refresh')});
+        event.target.addOptSelect.value = "";
+      }
+      else if(event.target.addWordOpt != undefined){
+        console.log(event.target.addWordOpt.value);
+        Meteor.call('addOptWordData', Session.get('selectedSlide'), event.target.addWordOpt.value, Session.get('addWordForm'), function(result, error){$('.selectpicker').selectpicker('refresh')});
+        Session.set('addWordForm', null);
+        event.target.addWordOpt.value = "";
+      }
+      else if(event.target.insertWord != undefined){
+        console.log(event.target.insertWord.value);
+        Meteor.call('insertWordData', Session.get('selectedSlide'), event.target.insertWord.value, Session.get('insertWordForm'), function(result, error){$('.selectpicker').selectpicker('refresh')});
+        Session.set('insertWordForm', null);
+        event.target.insertWord.value = "";
+      }
+      else{
+        console.log("submit non reconnu... :/");
+      }
+    },
+    'click .close': function(event){
+      console.log(event);
+      var idWord = $('.selectpicker').find(":selected")[0].id;
+      console.log(idWord);
+      Meteor.call('removeWordData', idWord, function(result, error){console.log(result);$('.selectpicker').selectpicker('refresh')});
     },
   })
 
 
  Template.addWordOpt.events({
-  'submit form': function(event){
-    event.preventDefault();
-    Meteor.call('addOptWordData', Session.get('selectedSlide'), event.target.addOpt.value, Session.get('wordToAddOpt'));
-    event.target.addOpt.value = "";
+  'click form': function(){
+    console.log(this.var[0]);
+    Session.set('addWordForm', this.var[0]);
   },
  })
 
 
- Template.addWord.helpers({
-    addWordButton: function(idWord){
-      return ((idWord == Session.get('mouseOverWord')) && (idWord != Session.get('addWordForm')));
+ Template.insertWord.helpers({
+    insertWordForm: function(idWord){
+      return ((idWord == Session.get('mouseOverWord')) && (Session.get('addWordForm')==null));
     },
-    addWordForm: function(idWord){
-      return ((idWord == Session.get('addWordForm')) && (idWord == Session.get('mouseOverWord')));
-    },
-    wordBefore: function(idWord){
-      Session.set('wordBefore', idWord);
-    },
-    reinitialiseAddWord: function(){
-      Session.set('addWordForm', null);
-      Session.set('wordToAddOpt', null);
+    reinitialiseInsertWord: function(){
+      Session.set('insertWordForm', null);
     }
  })
 
- Template.addWord.events({
-  'click #addWordButton': function(){
-    Session.set('addWordForm', Session.get('wordBefore').toString());
-  },
-  'submit form': function(){
-    event.preventDefault();
-    Meteor.call('insertWordData', Session.get('selectedSlide'), event.target.addWord.value, Session.get('addWordForm'));
-    event.target.addWord.value = "";
-    Session.set('addWordForm', null);
+ Template.insertWord.events({
+  'click form': function(){
+    Session.set('insertWordForm', this.var[0]);
   },
  })
 
 
   Template.pushWord.helpers({
-    pushWordButton: function(slide){
-      return (slide==Session.get('mouseOverSlide') && !Session.get('pushWordForm'));
-    },
     pushWordForm: function(slide){
-      return (slide==Session.get('mouseOverSlide') && Session.get('pushWordForm'));
+      return (slide==Session.get('mouseOverSlide') && (Session.get('addWordForm')==null) && (Session.get('insertWordForm')==null));
     },
  })
 
  Template.pushWord.events({
-  'click #pushWordButton': function(){
-    Session.set('pushWordForm', true);
-  },
   'submit form': function(){
     event.preventDefault();
     Meteor.call('pushWordData', Session.get('selectedSlide'), event.target.pushWord.value);
     event.target.pushWord.value = "";
-    Session.set('pushWordForm', false);
+  },
+ })
+
+ Template.displayRemove.helpers({
+  wordFromCurrentUser: function(idWord){
+    return WordsCollection.findOne(idWord.toString()).author == Meteor.userId();
   },
  })
 
@@ -244,11 +286,14 @@ if (Meteor.isServer) {
     },
     'addOptWordData': function(idSlide, textVar, idWordDest){
       if((idSlide!=undefined) && (idWordDest!=undefined)){
-        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide});
+        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide, author:Meteor.userId()});
         var tab = SlidesCollection.findOne(idSlide).notes;
+        console.log(idWordDest);
         for(i=0; i<tab.length;i++){
-          if(tab[i] == idWordDest){
-            tab[i].push(idWordNewOpt);
+          for(j=0; j<tab[i].length;j++){
+            if(tab[i][j] == idWordDest){
+              tab[i].push(idWordNewOpt);
+            }
           }
         }
         SlidesCollection.update(
@@ -261,18 +306,18 @@ if (Meteor.isServer) {
     },
     'insertWordData': function(idSlide, textVar, idWordDest){
       if((idSlide!=undefined) && (idWordDest!=undefined)){
-        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide});
+        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide, author:Meteor.userId()});
         var tab = SlidesCollection.findOne(idSlide).notes;
-        tab.forEach(function (words, index) {
-          for(idWord of words){
-            if(idWord == idWordDest){
-              SlidesCollection.update(
-                {_id: idSlide}, 
-                {$push: {'notes': {$each: [[idWordNewOpt]], $position: index}}}
-              );
-              break;
-            }
+        tab.forEach(function (words, index){
+        for(idWord of words){
+          if(idWord == idWordDest){
+            SlidesCollection.update(
+              {_id: idSlide}, 
+              {$push: {'notes': {$each: [[idWordNewOpt]], $position: index}}}
+            );
+            break;
           }
+        }
         });
       }else{
         console.log("Wrong idSlide "+idSlide+" or idWordDest "+idWordDest);
@@ -280,8 +325,7 @@ if (Meteor.isServer) {
     },
     'pushWordData': function(idSlide, textVar){
       if(idSlide!=undefined){
-        var currentUserId = Meteor.userId();
-        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide, author:currentUserId});
+        var idWordNewOpt = WordsCollection.insert({text:textVar, score:100, slide:idSlide, author:Meteor.userId()});
         SlidesCollection.update(
           {_id: idSlide}, 
           {$push: {'notes': [idWordNewOpt]}}
@@ -313,9 +357,40 @@ if (Meteor.isServer) {
         console.log("Wrong idCourse "+idCourse);
       }
     },
+    'removeSlideData': function(idSlide){
+      if(idSlide!=undefined){
+        var currentUserId = Meteor.userId();
+        WordsCollection.remove({slide:idSlide});
+        SlidesCollection.remove(idSlide);
+      }else{
+        console.log("Wrong idSlide "+idSlide);
+      }
+    },
+    'removeWordData': function(idWord){
+      if(idWord!=undefined){
+        var idSlide = WordsCollection.findOne(idWord).slide;
+        var tab = SlidesCollection.findOne(idSlide).notes;
+        WordsCollection.remove(idWord);
+        tab.forEach(function (words, indexI){
+          words.forEach(function (word, indexJ){
+            if(word.toString() == idWord.toString()){
+              console.log(indexI);
+              console.log(indexJ);
+              tab[indexI].splice(indexJ, 1);
+            }
+          });
+        });
+        SlidesCollection.update(
+          {_id: idSlide}, 
+          {$set: {'notes': tab}}
+        );
+      }else{
+        console.log("Wrong idWord "+idWord);
+      }
+    },
   });
 
-}
+} 
 
 CoursesCollection = new Mongo.Collection('courses');
 SlidesCollection = new Mongo.Collection('slides');
