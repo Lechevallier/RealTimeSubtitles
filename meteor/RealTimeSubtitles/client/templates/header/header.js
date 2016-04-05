4175a34192e3408b37c7b7637e463701d7936908
Lechevallier <maxime.lechevallier@laposte.net>
2016-04-05 12:31:35 +0200

    ajout de restriction d'accès au Reveal/Note

--- meteor/RealTimeSubtitles/client/templates/header/header.js
+++ meteor/RealTimeSubtitles/client/templates/header/header.js
@@ -1,6 +1,9 @@
 Template.header.helpers({
 	'isTeacher' : function(){
 		return CoursesCollection.findOne(Session.get('joinedCourse')).author == Meteor.userId();
+	},
+	'isJoined' : function(){
+		return CoursesCollection.find(Session.get('joinedCourse')).fetch().toString() != [];
 	}
 
 })
